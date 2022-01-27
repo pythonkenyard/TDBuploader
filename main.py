1df5f2845b1b1027662b45c51493706a28eccbaa
pythonkenyard <77246641+pythonkenyard@users.noreply.github.com>
2022-01-27 23:17:56 +0000

    Update main.py
    Added support for screenshotting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter.messagebox as mb
 from config import config as cfg
 import tdb
 import os, sys
+import cv2
 import json
 from pathlib import Path
 
@@ -38,13 +39,11 @@ if files[0].endswith(('.mp4', '.avi',"mkv")) ==True:
     mediafile=files[0]
 #todo add support for morefiles
 
-
 mediainfo = os.popen(r"binaries\mediainfo\MediaInfo.exe " +'"'+folloc+"/"+mediafile+'"').read()
 
-
-print("media info\n"+mediainfo)
+print("grabbed media info")
+#print("media info\n"+mediainfo)
 cwd = os.getcwd()
-
 
 startdigit = folloc.rfind("/", 0, len(folloc))
 print(startdigit)
@@ -54,3 +53,37 @@ os.system(r'torf "'+str(folloc)+'" -t '+cfg.tracker+ ' -M --private --out "'+str
 mediainfoutput = open(cwd+'/torrents/'+str(torrentname)+'.txt',"w")
 mediainfoutput.write(mediainfo)
 print("torrent and mediainfo written to /torrents as " +torrentname)
+
+cam = cv2.VideoCapture(folloc+"/"+mediafile) 
+
+print("capturing screens")
+# frame
+frame_number = 0
+screens =0
+
+while (True):
+    cam.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    ret, frame = cam.read()
+
+    if ret:
+        if screens ==5:
+            break
+        else:
+            frame_number=frame_number+500
+            # if video is still left continue creating images
+            name = './torrents' + str(torrentname)+str(frame_number) + '.jpg'
+            print('Creating...' + name)
+
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+
+            # increasing counter so that it will
+            # show how many frames are created
+            frame_number += 2500
+            screens +=1
+    else:
+        break
+
+# Release all space and windows once done
+cam.release()
+cv2.destroyAllWindows()
