91af62473830ed721d0ba88cc55a86eb3c364b17
pythonkenyard <77246641+pythonkenyard@users.noreply.github.com>
2022-03-02 21:37:58 +0000

    Support for auto upload to TDB

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ from pathlib import Path
 from tdb import *
 from tkinter import *
 
+print(" _______                        _\n \
+|__   __|                      | |\n \
+   | | ___  _ __ _ __ ___ _ __ | |_ ___ _ __ \n \
+   | |/ _ \\| '__| '__/ _ \\ '_ \\| __/ _ \\ '__| \n \
+   | | (_) | |  | | |  __/ | | | ||  __/ |\n \
+   |_|\\___/|_|  |_|  \\___|_| |_|\\__\\___|_| ")
+
+print("\nVersion 0.0.4,\n https://github.com/pythonkenyard/TDBuploader ")
+
+
 with open("config/config.yaml", 'r') as stream:
     cfg = yaml.safe_load(stream)
 #print(str(cfg))
@@ -29,40 +39,42 @@ except:
 total = 1
 torrentlist=[]
 
-folloc, selection, cfg = selectfolder(selection, cfg)
+while selection != "6":
+    selection = "0"
+    folloc, selection, cfg = selectfolder(selection, cfg)
 
 
-if int(selection) > 1:
-    topfolder = Folder(folloc)
-    print("Folder directory is " + topfolder.directory  + " \nFolder name is "+topfolder.name)
-else:
-    print("Single file to be uploaded")
-    torrentlist.append(folloc)
-    
-if selection == "2" or selection == "4":
-    topfolder.content = os.listdir(folloc)
+    if int(selection) > 1:
+        topfolder = Folder(folloc)
+        print("Folder directory is " + topfolder.directory  + " \nFolder name is "+topfolder.name)
+    else:
+        print("Single file to be uploaded")
+        torrentlist.append(folloc)
 
-    for i in topfolder.content:
-        folfolloc = folloc+"/"+str(i)
-        if selection == "4":
-            topfolder.i = Folder(folfolloc)
-            topfolder.i.content = os.listdir(folfolloc)
-        else:
-            topfolder.i = File(folfolloc)
-        print(str(total)+"."+"Cued to create torrent "+ folfolloc)
-        
-        if topfolder.i.type == "folder":
-            for item in topfolder.i.content:
-                print("Content of torrent is "+ str(item) + ".")
-        else:
-             print("Single file")            
-        total +=1
-        torrentlist.append(folfolloc)
-    print("Total torrents to create is "+str(total))
+    if selection == "2" or selection == "4":
+        topfolder.content = os.listdir(folloc)
 
-elif selection == "3":
-    torrentlist.append(folloc)
-    
-print("Creating torrent..")     
-for torrent in torrentlist:
-    createtorrent(torrent, selection)
+        for i in topfolder.content:
+            folfolloc = folloc+"/"+str(i)
+            if selection == "4":
+                topfolder.i = Folder(folfolloc)
+                topfolder.i.content = os.listdir(folfolloc)
+            else:
+                topfolder.i = File(folfolloc)
+            print(str(total)+"."+"Cued to create torrent "+ folfolloc)
+
+            if topfolder.i.type == "folder":
+                for item in topfolder.i.content:
+                    print("Content of torrent is "+ str(item) + ".")
+            else:
+                 print("Single file")
+            total +=1
+            torrentlist.append(folfolloc)
+        print("Total torrents to create is "+str(total))
+
+    elif selection == "3":
+        torrentlist.append(folloc)
+
+    print("Creating torrent..")
+    for torrent in torrentlist:
+        createtorrent(torrent, selection)
