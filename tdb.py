98ea8ede2d06fcdc28db4e3f488f16a803c1e8b1
pythonkenyard <77246641+pythonkenyard@users.noreply.github.com>
2022-06-25 10:13:39 +0100

    Update tdb.py

--- tdb.py
+++ tdb.py
@@ -15,6 +15,8 @@ import trackers.bhd as bhd
 from guessit import guessit
 import imdb
 import requests
+import tvdb
+
 
 with open("config/config.yaml", 'r') as stream:
     cfg = yaml.safe_load(stream)
@@ -35,9 +37,6 @@ except:
     os.system("cls")
     print("\nWARNING! no qbittorrent config file or Broken config file\nPlease download the lastest config file to config/chrome.yaml")
     time.sleep(3)
-
-#todo - media parsing additional checks for title naming.
-#todo - check against imdb/tvdb/tmdb for short_title
 
 uploadtrackers = ["bhd","beyondhd","tdb","torrentdb"]
 """requirements for new tracker
@@ -136,14 +135,14 @@ Other options:\n\
                             apikey = "N/A"
                     else:
                         autoupload = False
-                        releasegrp = "-NoGrp"
                         usr = "N/A"
                         pwd = "N/A"
                         apikey = "N/A"
+                        releasegrp = "-NoGrp"
                 else:
                     usr = "N/A"
                     pwd = "N/A"
-                    releasegrp = "-NpGrp"
+                    releasegrp = "-NoGrp"
                     apikey = "N/A"
                     print(f"Autoupload to tracker set to false.\nNote it's Only supported for trackers {uploadtrackers}")
                     autoupload = False
@@ -312,7 +311,7 @@ def selectfolder(selection, cfg):
             root.withdraw()
             if selection == "0" or selection == "5":
                 selection = input(
-            "Please select your upload type from 1-4 or enter setup\n\n \
+            "Please select your upload type from 1-4 or 5-6 for Settings\n\n \
 File(s) Upload:\n\
 (1)Single file.\n\
 (2)Multiple files as multiple separate torrents (Select folder containing files)\n\n \
@@ -539,11 +538,15 @@ def imdbtag(title):
     ia = imdb.Cinemagoer()
     try:
         movies = ia.search_movie(title)
-        if len(movies) <2:
+        #print(str(movies))
+        if len(movies) <1:
+            print("no result from IMDB check")
+            raise
+        elif len(movies) == 1:
             print("one result")
             imdbnum = movies[0].movieID
             imdbtitle = movies[0]["title"]
-            imdbyear = movies[0]["title"]
+            imdbyear = movies[0]["year"]
         elif len(movies)>1:
             possiblemovie = 1
             printtable = [["No.","Year","Title","IMDb"]]
@@ -553,43 +556,78 @@ def imdbtag(title):
                 try:
                     year = movies[selectchoice]["year"]
                 except:
-                    "0000"
+                    year = "0000"
                 title = movies[selectchoice]["title"]
                 id = movies[selectchoice].movieID
                 printtable.append([f"({possiblemovie})",str(year),title,str(id)])
                 possiblemovie += 1
-            for row in printtable:
-                print("{: <4} {: <5} {: <30} {: <8}".format(*row))
-            print(f"({possiblemovie}) - NO MATCH")
-            choice = int(input(f"Select correct option 1-{possiblemovie}: ")) -1
-            try:
-                imdbnum = movies[choice].movieID
-                imdbtitle = movies[choice]["title"]
-                imdbyear = movies[choice]["title"]
-            except:
-                print("failed to assign")
-                imdbnum, imdbtitle, imdbyear = "1", "None", "0000"
+            x="n"
+            while x == "n":
+                x="y"
+                print("\nIMDB QUERY TABLE\n")
+                for row in printtable:
+                    print("{: <4} {: <5} {: <35} {: <10} ".format(*row))
+                print(f"({possiblemovie}) - NO MATCH")
+                choice = int(input(f"Select correct option 1-{possiblemovie}: ")) -1
+                try:
+                    imdbnum = movies[choice].movieID
+                    imdbtitle = movies[choice]["title"]
+                    imdbyear = movies[choice]["title"]
+                    try:
+                        movie = ia.get_movie(imdbnum, info=['taglines', 'plot'])
+
+                        try:
+                            plot = movie['plot'][0]
+                            print("\nPLOT\n"+plot)
+                        except:
+                            print("Plot unknown")
+                        try:
+                            synopsis = movie['synopsis'][0]
+                            print("\nSYNOPSIS\n"+synopsis)
+                        except:
+                            print("Synopsis unknown")
+                        x=input("is this correct?[y/n").lower()
+                        if x == "n":
+                            os.system("cls")
+                        elif x == "y":
+                            continue
+                        else:
+                            print("Invalid selection")
+                            time.sleep(1)
+                            x = "n"
+                    except:
+                        pass
+                except:
+                    print("failed to assign IMDB reference")
+                    imdbnum, imdbtitle, imdbyear = "1", "None", "0000"
+
+
     except:
-        print("No IMDB Match")
-        imdbnum, imdbtitle, imdbyear = "1", "None", "0000"
+        try:
+            #todo add support for searching and adding multiple from here
+            title = title.replace(" ", "%20")
+            response = requests.get(rf"https://imdb-api.com/en/API/SearchSeries/k_p1b27972/{title}").json()
+            print("this is the tv search "+str(response))
+            imdbnum = response["results"]["0"]["id"]
+            imdbtitle = response["results"]["0"]["title"]
+            imdbyear = response["results"]["0"]["description"]
+        except:
+            print("No IMDB Match")
+            imdbnum, imdbtitle, imdbyear = "1", "None", "0000"
+
     return imdbnum, imdbtitle, imdbyear
 
-def guess(torrentname):
+def guess(filetitle):
     try:
-        filetitle = guessit(torrentname)['title']
-        #imdb
-        try:
-            print(filetitle+" is the title")
-            imdbnum, imdbtitle, imdbyear = imdbtag(filetitle)
-            print(f"{imdbnum}, {imdbtitle}, {imdbyear}")
-        except:
-            print("cannot identify IMDB id")
-        return imdbnum, imdbtitle, imdbyear, filetitle
+        imdbnum, imdbtitle, imdbyear = imdbtag(filetitle)
+        print(f"{imdbnum}, {imdbtitle}, {imdbyear}")
     except:
-        print("didnt find torrent title")
+        print("cannot identify IMDB id")
+    return imdbnum, imdbtitle, imdbyear
+
 
 def tmdbtag(imdbnum,filetitle,tmdb_api_key):
-    #https://api.themoviedb.org/3/find/tt0805418?api_key=5a81ec2f2a4be60f135473528d2965cf&language=en-US&external_source=tvdb_id
+    #f'https://api.themoviedb.org/3/find/tt0805418?api_key={tmdb_api_key}&language=en-US&external_source=tvdb_id'
     try:
         tmdbrequest = requests.get(url=f"https://api.themoviedb.org/3/find/tt{imdbnum}?api_key={tmdb_api_key}&language=en-US&external_source=imdb_id").json()
         #print(tmdbrequest)
@@ -623,24 +661,32 @@ def tmdbtag(imdbnum,filetitle,tmdb_api_key):
             tvdivider = tmdbselection
             print(str(tvdivider))
             for row in tmdblist:
-                print("{: <4} {: <5} {: <30} {: <5} {: <40}".format(*row))
+                print("{: <4} {: <5} {: <30} {: <5} {: <60}".format(*row))
             tmdbrequest2 = requests.get(url = f"https://api.themoviedb.org/3/search/tv?api_key={tmdb_api_key}&query={filetitle}").json()
             print("Possible tv matches")
-            for i in tmdbrequest2["results"]:
-                id = i["id"]
-                title = i["name"]
-                overview = i["overview"]
-                year = i["first_air_date"]
-                year = year[0:4]
-                overviewabbv = overview[:60]
-                tmdblist.append([f"({tmdbselection})",year, title, id, overviewabbv])
-                tmdblist2.append([f"({tmdbselection})",year, title, id, overviewabbv])
-                tmdbselection += 1
-            for row in tmdblist2:
-                print("{: <4} {: <5} {: <30} {: <5} {: <40}".format(*row))
-            print(f"({tmdbselection}) - NO MATCH")
+            try:
+                for i in tmdbrequest2["results"]:
+                    id = i["id"]
+                    title = i["name"]
+                    overview = i["overview"]
+                    year = i["first_air_date"]
+                    year = year[0:4]
+                    overviewabbv = overview[:60]
+                    tmdblist.append([f"({tmdbselection})",year, title, id, overviewabbv])
+                    tmdblist2.append([f"({tmdbselection})",year, title, id, overviewabbv])
+                    tmdbselection += 1
+
+                for row in tmdblist2:
+                    print("{: <4} {: <5} {: <30} {: <5} {: <40}".format(*row))
+                print(f"({tmdbselection}) - NO MATCH ABOVE")
+            except:
+                print("no matches")
             if tmdbselection > 1:
-                choice = int(input(f"Select matching TMDB option 1-{tmdbselection}: "))
+                if tmdbselection ==2:
+                    print("single result. Automatically assigning tmdb tv show id")
+                    choice = 1
+                else:
+                    choice = int(input(f"Select matching TMDB option 1-{tmdbselection}: "))
             else:
                 print("No matches on TMDB either.")
                 choice = 1
@@ -666,7 +712,10 @@ def tmdbtag(imdbnum,filetitle,tmdb_api_key):
             print("failed getting tmdb id. is your api key correct")
             tmdbid, tmdb_desc, tmdb_type = "1", "none", "none"
     print(f"TMDB Id found to be {tmdbid}")
+
     return tmdbid, tmdb_desc, tmdb_type
+
+
 
 def createtorrent(folloc, selection, cfg):
     folloc = folloc
@@ -729,23 +778,47 @@ def createtorrent(folloc, selection, cfg):
     torrentname = folloc[startdigit:len(folloc)]
 
     try:
+        fileattributes = guessit(torrentname)
+        filetitle = fileattributes['title']
+        print(filetitle+" is the title")
+        rlsgrp = fileattributes["release_group"]
         tmdb_api_key = chromecfg["tmdb_api_key"]
         if len(tmdb_api_key) > 1:
             try:
-                imdbnum, imdbtitle, imdbyear, filetitle = guess(torrentname)
+                imdbnum, imdbtitle, imdbyear = guess(filetitle)
                 #print(f"{imdbnum}, {imdbtitle}, {imdbyear}")
             except:
                 print("no imdbid found.")
             try:
                 #print(f"{imdbnum},{filetitle},{tmdb_api_key}")
-                tmdbnum, tmdb_desc, tvdb_type = tmdbtag(imdbnum,filetitle,tmdb_api_key)
+                tmdbnum, tmdb_desc, tmdb_type = tmdbtag(imdbnum,filetitle,tmdb_api_key)
             except:
                 print("skipping tmdb check also. please setup tmdb api key for searching IMDB and TMDB")
+            try:
+                episode = fileattributes['episode']
+                episode = str(episode).zfill(2)
+            except:
+                episode = "01"
+            try:
+                season = fileattributes['season']
+                season = str(season).zfill(2)
+            except:
+                season = " "
+            try:
+                seasonepisode = f"S{season}E{episode}"
+                #print(seasonepisode)
+                tvdbdata = tvdb.get_tvdb(filetitle,seasonepisode)
+                print("gathered tvdb data")
+                print(str(tvdbdata))
+            except:
+                print("no tvdb information available")
+                #print(str(torrentname))
     except:
-        imdbnum, imdbtitle, imdbyear, tmdbnum, tmdb_desc, tvdb_type = "1","None","1", "1", "none", "none"
+        imdbnum, imdbtitle, imdbyear, tmdbnum, tmdb_desc, tmdb_type = "1","None","1", "1", "none", "none"
         print("skipping use of TMDB/IMDB. To enable imdb/tmdb please add a tmdb config variable")
 
-#print(str(torrentname))
+
+
     newdir= f"{cwd}/torrents/{torrentname}"
     mediainfodir = f"{cwd}/torrents/aamediainfo"
     try:
@@ -819,7 +892,7 @@ def createtorrent(folloc, selection, cfg):
             else:
                 frame_number=frame_number+3000
                 # if video is still left continue creating images
-                name = f'{cwd}/torrents/{torrentname}/{torrentname}{frame_number}.png'
+                name = f'{cwd}/torrents/{torrentname}/{frame_number}.png'
                 print('Creating...' + name)
                 screenshots.append(name)
                 # writing the extracted images
@@ -851,14 +924,31 @@ def createtorrent(folloc, selection, cfg):
     downloadsource = check_source(torrentname,cfg["sourcelist"])
 
     #Add some media info to torrent posting if doing so
-    if tvdb_type == "tv":
+
+    if tmdb_type == "tv":
         description = f"TMDB\n[url=https://www.themoviedb.org/tv/{tmdbnum}]TMDB LINK[/url]\n\n{tmdb_desc}"
-    elif tvdb_type == "movie":
+    elif tmdb_type == "movie":
         description = f"TMDB\n[url=https://www.themoviedb.org/movie/{tmdbnum}]TMDB LINK[/url]\n\n{tmdb_desc}"
     else:
         description = " "
-    if int(imdbnum)>1:
-        description = f"{imdbyear}\n" +description+ f"\n\nIMDB\n[url=https://www.imdb.com/title/tt{imdbnum}]IMDB LINK[/url]"
+    try:
+        if int(imdbnum)>1:
+            description = f"{imdbyear}\n" +description+ f"\n\nIMDB\n[url=https://www.imdb.com/title/tt{imdbnum}]IMDB LINK[/url]"
+            print("appended IMDB info to description for upload")
+    except:
+        pass
+    try:
+        tvdbid = str(tvdbdata[0])
+        if len(tvdbid)>2:
+            print("adding tvdbid information")
+            tvdbepinfo = tvdbdata[3]
+            tvdbepjpg = tvdbdata[4]
+            tvdbserinfo = tvdbdata[1]
+            tvdbserjpg = tvdbdata[2]
+            description = f"{filetitle} - {seasonepisode}\n\n{tvdbepinfo}\n[img]{tvdbepjpg}[/img]\n\nTVDB SHOW INFORMATION\n{tvdbserinfo}\n[img]{tvdbserjpg}[/img]\n" +description
+            print("appended TVDB info to description for upload")
+    except:
+        pass
     if len(uploadlist) >0:
         for y in uploadlist.items():
             #{tracker:torrent}, screenshots, title name, duration, height, audio format, video format
@@ -884,6 +974,7 @@ def createtorrent(folloc, selection, cfg):
                     tdb = torrentdb.tdb(track_loc,screenshots, torrentname, duration, title_height, audioformat,videoformat, mediainfowrite,usr,pwd,tag)
 
                     short_title, seasonepisode, seasonmatch, rlsgrp = tdb.get_short_title()
+                    seasonmatch = [season,season]
                     videosource, videosource2 = tdb.get_type()
                     if len(rlsgrp)>1:
                         tdb.releasegrp = "-"+rlsgrp
