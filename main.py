a07408ebd1eb6f1997392fb9d0a7b8372f874ea5
pythonkenyard <77246641+pythonkenyard@users.noreply.github.com>
2022-06-25 10:10:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ print("\n\
                             |_|")
 print("\nVersion 2.6.0 Beta,\nNote it is suggested to use latest table release.\nhttps://github.com/pythonkenyard/TDBuploader ")
 
-def initial_setup():
+def initial_setup(cfg):
 
     print(f"note supported trackers for auto upload\n{uploadtrackers}")
     trackername = input("\nPlease name your tracker e.g. 'torrentdb': ")
@@ -47,52 +47,52 @@ def initial_setup():
                 usr = str(input("Please enter your login name: "))
                 pwd = str(input("Please enter your password: "))
                 apikey = "N/A"
-            setupselection = "6"
-            while setupselection == "6":
-                with open("config/chrome.yaml", 'r') as stream2:
-                    chromecfg = yaml.safe_load(stream2)
-                print("selecting a chrome profile")
-                appdata = os.getenv('localappdata')
-                profile = 5
-                isExist = False
-                while isExist == False:
-                    isExist  = os.path.exists(fr"{appdata}\Google\Chrome\User Data\Profile {profile}")
-                    profile -=1
-                    if profile == 0:
-                        setprofile = "Default"
-                        isExist = True
-                    else:
-                        setprofile = "Profile " + str(profile+1)
-                        print("Configuring Chrome settings for autoupload\nPossible options:")
-                print(f"picked profile {setprofile}")
 
-                cwd = os.getcwd()
-                download_directory = f"{cwd}/downloads"
-                print(f"download directory set to {download_directory}")
-                tmdbapi = input("[optional] Input TMDB API key to enable metadata scraping from TMDB,IMDB,TVDB: ")
-
-                chromecfg["profilename"] = setprofile
-                chromecfg["tmdb_api_key"] = tmdbapi
-                chromecfg["downloadlocation"] = download_directory
-                with open('config/chrome.yaml','w') as yamlfile:
-                    yaml.safe_dump(chromecfg, yamlfile)
-                    yamlfile.close()
-                prog_files = os.getenv("ProgramFiles")
-                qbit_check = os.path.exists(f"{prog_files}/qBittorrent/qbittorrent.exe")
-                if qbit_check:
-                    qbit_loc = f"{prog_files}/qBittorrent/qbittorrent.exe"
-                    print(f"Qbittorrent location set as {qbit_loc}")
+            with open("config/chrome.yaml", 'r') as stream2:
+                chromecfg = yaml.safe_load(stream2)
+            print("selecting a chrome profile")
+            appdata = os.getenv('localappdata')
+            profile = 5
+            isExist = False
+            print("Checking possible Chrome settings for autoupload:")
+            while isExist == False:
+                isExist  = os.path.exists(fr"{appdata}\Google\Chrome\User Data\Profile {profile}")
+                profile -=1
+                if profile == 0:
+                    setprofile = "Default"
+                    isExist = True
                 else:
-                    qbit_loc = input("[optional]Please input qbittorrent location if you are using autoupload and wish to add uploaded torrents to qbittorrent: ")
-                with open("config/qbit.yaml", 'r') as stream3:
-                    qbitcfg = yaml.safe_load(stream3)
-                stream3.close()
-                if len(qbit_loc)>5:
-                    qbitcfg["Enabled"] = True
-                    qbitcfg["Qbittorrentlocation"] = qbit_loc
-                with open("config/qbit.yaml", 'w') as stream4:
-                    yaml.safe_dump(qbitcfg, stream4)
-                stream4.close()
+                    setprofile = "Profile " + str(profile+1)
+
+            print(f"Selected Chrome profile {setprofile}")
+
+            cwd = os.getcwd()
+            download_directory = f"{cwd}/downloads"
+            print(f"Uploaded torrents will be saved to {download_directory}\n")
+            tmdbapi = input("[optional] Input TMDB API key to enable metadata scraping from TMDB,IMDB,TVDB: ")
+
+            chromecfg["profilename"] = setprofile
+            chromecfg["tmdb_api_key"] = tmdbapi
+            chromecfg["downloadlocation"] = download_directory
+            with open('config/chrome.yaml','w') as yamlfile:
+                yaml.safe_dump(chromecfg, yamlfile)
+                yamlfile.close()
+            prog_files = os.getenv("ProgramFiles")
+            qbit_check = os.path.exists(f"{prog_files}/qBittorrent/qbittorrent.exe")
+            if qbit_check:
+                qbit_loc = f"{prog_files}/qBittorrent/qbittorrent.exe"
+                print(f"Qbittorrent location found to be {qbit_loc}")
+            else:
+                qbit_loc = input("[optional]Please input qbittorrent location if you are using autoupload and wish to add uploaded torrents to qbittorrent: ")
+            with open("config/qbit.yaml", 'r') as stream3:
+                qbitcfg = yaml.safe_load(stream3)
+            stream3.close()
+            if len(qbit_loc)>5:
+                qbitcfg["Enabled"] = True
+                qbitcfg["Qbittorrentlocation"] = qbit_loc
+            with open("config/qbit.yaml", 'w') as stream4:
+                yaml.safe_dump(qbitcfg, stream4)
+            stream4.close()
         else:
             autoupload = False
             usr = "N/A"
@@ -102,15 +102,13 @@ def initial_setup():
 
     screenshots = input("How many screenshots are required: ")
     trackerdata = {"announce": announce, "autotorrent" : autotorrent, "autorename" : autorename, "autoupload" : autoupload, "screenshots" : screenshots, "usr": usr, "pwd" : pwd, "releasegrp": releasegrp, "apikey": apikey}
+
     cfg["tracker"] = {trackername : trackerdata}
 
     with open('config/config.yaml','w') as yamlfile:
         yaml.safe_dump(cfg, yamlfile)
     print("Tracker info updated")
     yamlfile.close()
-    #reload config
-    with open("config/config.yaml", 'r') as stream:
-        cfg = yaml.safe_load(stream)
 
 
 
@@ -122,7 +120,8 @@ time.sleep(0.1)
 #initial setup, establish tracker etc..
 if cfg["tracker"] is None:
     print("\nFIRST TIME SETUP\n\n")
-    initial_setup()
+    initial_setup(cfg)
+    #reopen config
     with open("config/config.yaml", 'r') as stream:
         cfg = yaml.safe_load(stream)
 else:
